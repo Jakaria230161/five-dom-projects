e6d9e9d23a7c16d1c83b01729f8d010f2f728160
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-22 19:53:11 +0600

    html section done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log("connected with html");
+const playerArray = [];
+function display(playerProduct) {
+
+    let totalPrice = 0;
+    const tableBody = document.getElementById("cart-player");
+    tableBody.innerHTML = "";
+
+    for (let i =0; i < playerArray.length; i++){
+        const name = playerArray[i].playerName;
+        
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <tr>
+        <th>${i+1}</th>
+        <td>${name}</td>
+        `;
+
+        tableBody.appendChild(tr);
+    }
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+        <th></th>
+        <td>Total Price</td>
+        <td>${totalPrice}</td >
+    `;
+    tableBody.appendChild(tr);
+}
+
+function playerAddToCart(element) {
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+
+    const playerObj = {
+        playerName: productName,
+    }
+    playerArray.push(playerObj);
+    
+
+    document.getElementById("total-added-player").innerText = playerArray.length;
+    
+    display(playerArray);
+}
