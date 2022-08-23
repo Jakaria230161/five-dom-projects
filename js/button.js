69c353f626986b28aac27978ce53c24e5d050036
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-23 07:26:56 +0600

    alert message commit

--- js/button.js
+++ js/button.js
@@ -4,11 +4,16 @@ const playerCardArray = [];
 function getPlayer(playerCard) {
     const playerTable = document.getElementById("card-players");
     playerTable.innerHTML = "";
-    
-    
+
     for (let i = 0; i < playerCard.length; i++) {
 
         const player = playerCard[i].playerName;
+
+        if (playerCard.length > 5) {
+        
+            alert("Only five players are allowed to selecting in your team.")
+            return;
+        }
         
         const tr = document.createElement("tr");
         tr.innerHTML = `
@@ -31,7 +36,22 @@ function clickButton(element) {
     }
     playerCardArray.push(playerObject);
 
+
     document.getElementById("selected-list").innerText = playerCardArray.length;
     getPlayer(playerCardArray);
 } 
-// function start here 
+//<-------------------- function end here---------------------->//
+
+// let buttonDisabled = document.getElementById("clickButton");
+//     buttonDisabled.classList.add("disabled");
+
+//   if (playerCard.length > 5) {
+//     clickButton.disabled = false;
+//     }
+//     else {
+//     clickButton.disabled = true;
+// }
+
+//     playerCardArray(element){
+//     element.disabled = true;
+// }
